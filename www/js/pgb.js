f53b65244d89020a35a7ae1b538a39f4ee7f2298
4kiru <Mateusz.salawa@gmail.com>
2017-03-20 15:14:21 +0100

    wef

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,5 +1,9 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  
+    
 function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
+alert('Acceleration X: ' + acceleration.x + '\n' +
           'Acceleration Y: ' + acceleration.y + '\n' +
           'Acceleration Z: ' + acceleration.z + '\n' +
           'Timestamp: '      + acceleration.timestamp + '\n');
@@ -8,7 +12,13 @@ function onSuccess(acceleration) {
 function onError() {
     alert('onError!');
 }
+    
+     navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+}
 
-var options = { frequency: 3000 };  // Update every 3 seconds
 
-var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+
+
+
+
+        
